1eb637653616ffe3de5706762f7e36d525e981ad
josuerushanika <josuerushanika@gmail.com>
2023-10-19 23:52:04 +0200

    Update files

--- todo-list/src/index.tsx
+++ todo-list/src/index.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 interface Props {
-   color?: string;
+  color?: string;
 }
 
-class App extends React.Component <Props> {
+class App extends React.Component<Props> {
+  state = { counter: 0 };
+  onUp = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDown = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div> TEST</div>;
+    return (
+      <div>
+        <button onClick={this.onUp}>+</button>
+        {this.state.counter}
+        <button onClick={this.onDown}>-</button>
+      </div>
+    );
   }
 }
 
